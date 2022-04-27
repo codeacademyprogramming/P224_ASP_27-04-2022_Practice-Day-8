200b4ab46041cd5b69cce4f6b3d587a8375abc00
Hamid V. Mammadov <Hamid.mammadov@code.local>
2022-04-27 17:36:36 +0400

    First Commit

---
+++ P224Allup/Areas/Manage/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using P224Allup.Models;
+using P224Allup.ViewModels.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P224Allup.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController( UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid) return View();
+
+            AppUser appUser = await _userManager.FindByEmailAsync(loginVM.Email);
+
+            if (appUser == null)
+            {
+                ModelState.AddModelError("", "Email Or Password Is InCorrect");
+                return View();
+            }
+            Microsoft.AspNetCore.Identity.SignInResult signInResult = await _signInManager.PasswordSignInAsync(appUser, loginVM.Password, loginVM.RememberMe, true);
+
+            if (!signInResult.Succeeded)
+            {
+                ModelState.AddModelError("", "Email Or Password Is InCorrect");
+                return View();
+            }
+
+            if (signInResult.IsLockedOut)
+            {
+                ModelState.AddModelError("", "Hesabiniz Blocklanib");
+                return View();
+            }
+
+            return RedirectToAction("index", "Dashboard");
+        }
+    }
+}
---
+++ P224Allup/Controllers/AccountController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using P224Allup.Models;
+using P224Allup.ViewModels.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P224Allup.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid) return View();
+
+            AppUser appUser = new AppUser
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.Email,
+                UserName = registerVM.UserName
+            };
+
+            IdentityResult identityResult =  await _userManager.CreateAsync(appUser, registerVM.Password);
+
+            if (!identityResult.Succeeded)
+            {
+                foreach (var item in identityResult.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+                return View();
+            }
+
+            await _userManager.AddToRoleAsync(appUser, "Member");
+
+            await _signInManager.SignInAsync(appUser,true);
+
+            return RedirectToAction("index", "home");
+        }
+
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid) return View();
+
+            AppUser appUser = await _userManager.FindByEmailAsync(loginVM.Email);
+
+            if (appUser == null)
+            {
+                ModelState.AddModelError("", "Email Or Password Is InCorrect");
+                return View();
+            }
+            Microsoft.AspNetCore.Identity.SignInResult signInResult = await _signInManager.PasswordSignInAsync(appUser, loginVM.Password,loginVM.RememberMe,true);
+
+            if (!signInResult.Succeeded)
+            {
+                ModelState.AddModelError("", "Email Or Password Is InCorrect");
+                return View();
+            }
+
+            if (signInResult.IsLockedOut)
+            {
+                ModelState.AddModelError("", "Hesabiniz Blocklanib");
+                return View();
+            }
+
+            return RedirectToAction("index", "home");
+        }
+
+        #region Create Role
+        //public async Task<IActionResult> CreateRole()
+        //{
+        //    await _roleManager.CreateAsync(new IdentityRole { Name = "SuperAdmin" });
+        //    await _roleManager.CreateAsync(new IdentityRole { Name = "Admin" });
+        //    await _roleManager.CreateAsync(new IdentityRole { Name = "Member" });
+
+        //    return Ok();
+        //}
+        #endregion
+    }
+}
